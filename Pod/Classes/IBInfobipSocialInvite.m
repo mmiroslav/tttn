9d767a827340937253a2b723e957338dd75e1c84
Miroslav Milivojevic <Miroslav.Milivojevic@infobip.com>
2014-12-09 17:01:07 +0100

    wip

--- Pod/Classes/IBInfobipSocialInvite.m
+++ Pod/Classes/IBInfobipSocialInvite.m
@@ -6,13 +6,6 @@
 //  Copyright (c) 2014 Infobip. All rights reserved.
 //
 
-#import "InfobipSocialInvite.h"
-#import "IBSIAddressBook.h"
-#import "IBSIInvite.h"
-
-#define IBSI_PATH_SEND_INVITE @"1/social-invite/invitation"
-#define IBSI_EXPIRED_DELIVERY_MESSAGE_ERROR @"Message not found for specified request ID"
-
 @implementation InfobipSocialInvite
 
 
